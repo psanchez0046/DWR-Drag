155a168e2e6d4c238b7d19e00b272d5e4e4158c6
Pablo Sánchez-Puga <66384512+psanchez0046@users.noreply.github.com>
2025-07-26 20:40:59 +0200

    Update GetFilenames.m

--- MATLAB/GetFilenames.m
+++ MATLAB/GetFilenames.m
@@ -1,17 +1,14 @@
 function [ names_cell ] = GetFilenames(pattern, inputDataFilepath)
-% This function returns a cell array with the names of the files 
-% having the specific pattern contained in the specified filepath
+% Returns a cell array of filenames from a directory that match a pattern.
+dinfo = dir(inputDataFilepath);             % Get directory info
+all_names = {dinfo.name};                   % Extract all file names
+names_cell = {};                            % Initialize output cell array
+idx = 1;
 
-dinfo = dir(inputDataFilepath);
-names_cell = {dinfo.name};
-aux=0;
-names_cell_aux = names_cell;
-for i=1:length(names_cell)
-    a=strfind(names_cell(i),pattern);
-    if a{1,1}~=0
-        aux=aux+1;
-        names_cell_aux(aux)=names_cell(i);
+for i = 1:length(all_names)
+    if contains(all_names{i}, pattern)      % Use 'contains' instead of strfind
+        names_cell{idx} = all_names{i};     % Add matching filename
+        idx = idx + 1;
     end
-end    
-names_cell=names_cell_aux(1:aux);
+end
 end
